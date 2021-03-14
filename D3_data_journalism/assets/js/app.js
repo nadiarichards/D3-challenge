b07d4132e9530441d3dfb4ce127eef41068461b5
nadiarichards <73898387+nadiarichards@users.noreply.github.com>
2021-03-14 14:57:40 -0400

    d3.extent

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -16,7 +16,7 @@ d3.csv("data/data.csv", function(data) {
 
   // Add X axis
   var x = d3.scaleLinear()
-    .domain([0, 4000])
+    .domain(d3.extent(data, d => d.value))
     .range([ 0, width ]);
   svg.append("g")
     .attr("transform", "translate(0," + height + ")")
@@ -24,7 +24,7 @@ d3.csv("data/data.csv", function(data) {
 
   // Add Y axis
   var y = d3.scaleLinear()
-    .domain([0, 500000])
+    .domain(d3.extent(data, d => d.value))
     .range([ height, 0]);
   svg.append("g")
     .call(d3.axisLeft(y));
