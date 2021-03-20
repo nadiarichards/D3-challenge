52bd3953ab8db1064d2aa7b85886dd3fdc957e63
nadiarichards <73898387+nadiarichards@users.noreply.github.com>
2021-03-19 22:49:42 -0400

    axis

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -3,9 +3,9 @@ const xLabel = "In Poverty (%)";
 const yValue = d => d.healthcare;
 const yLabel = "Lacks Healthcare (%)";
 
-var margin = {top: 20, right: 30, bottom: 60, left: 60},
-    width = 600 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+var margin = {top: 20, right: 60, bottom: 100, left: 60},
+    width = 700 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("#scatter")
@@ -31,19 +31,26 @@ d3.csv("assets/data/data.csv").then (function(data) {
     .range([ 0, width ]);
   var xAxisG = svg.append("g")
     .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
-//   xAxisG.append('text')
-//     .attr('class', 'axis-label')
-//     .attr('x', innerWidth / 2)
-//     .attr('y', 100)
-//     .text(xLabel);
+    .call(d3.axisBottom(x))
+    .append('text')
+    .attr('class', 'axis-label')
+    .attr('x', innerWidth / 2)
+    .attr('y', 100)
+    .text(xLabel);
 
   // Add Y axis
   var y = d3.scaleLinear()
     .domain(d3.extent(data, yValue))
     .range([ height, 0]);
   var yAxisG = svg.append("g")
-    .call(d3.axisLeft(y));
+    .call(d3.axisLeft(y))
+    .append('text')
+    .attr('class', 'axis-label')
+    .attr('x', -innerHeight / 2)
+    .attr('y', -60)
+    .attr('transform', `rotate(-90)`)
+    .style('text-anchor', 'middle')
+    .text(yLabel);
 
   // Add dots
   svg.append('g')
@@ -54,5 +61,116 @@ d3.csv("assets/data/data.csv").then (function(data) {
         .attr("cx", function (d) { return x(d.poverty); } )
         .attr("cy", function (d) { return y(d.healthcare); } )
         .attr("r", 5)
-        .style("fill", "lightblue")
+        .style("fill", "rgb(143,194,217)")
 });
+
+// var width = parseInt(d3.select("#scatter").style("width"));
+// var height = width - width / 3.9;
+// var margin = 20;
+// var labelArea = 110;
+// var tPadBot = 40;
+// var tPadLeft = 40;
+// var svg = d3
+//   .select("#scatter")
+//   .append("svg")
+//   .attr("width", width)
+//   .attr("height", height)
+//   .attr("class", "chart");
+// var circRadius;
+// function crGet() {
+//   if (width <= 530) {
+//     circRadius = 5;
+//   }
+//   else {
+//     circRadius = 10;
+//   }
+// }
+// crGet();
+// // A) Bottom Axis
+// // ==============
+// // We create a group element to nest our bottom axes labels.
+// svg.append("g").attr("class", "xText");
+// // xText will allows us to select the group without excess code.
+// var xText = d3.select(".xText");
+// // We give xText a transform property that places it at the bottom of the chart.
+// // By nesting this attribute in a function, we can easily change the location of the label group
+// // whenever the width of the window changes.
+// function xTextRefresh() {
+//   xText.attr(
+//     "transform",
+//     "translate(" +
+//       ((width - labelArea) / 2 + labelArea) +
+//       ", " +
+//       (height - margin - tPadBot) +
+//       ")"
+//   );
+// }
+// xTextRefresh();
+// // Now we use xText to append three text SVG files, with y coordinates specified to space out the values.
+// // 1. Poverty
+// xText
+//   .append("text")
+//   .attr("y", -26)
+//   .attr("data-name", "poverty")
+//   .attr("data-axis", "x")
+//   .attr("class", "aText active x")
+//   .text("In Poverty (%)");
+// // 2. Age
+// xText
+//   .append("text")
+//   .attr("y", 0)
+//   .attr("data-name", "age")
+//   .attr("data-axis", "x")
+//   .attr("class", "aText inactive x")
+//   .text("Age (Median)");
+// // 3. Income
+// xText
+//   .append("text")
+//   .attr("y", 26)
+//   .attr("data-name", "income")
+//   .attr("data-axis", "x")
+//   .attr("class", "aText inactive x")
+//   .text("Household Income (Median)");
+// // B) Left Axis
+// // ============
+// // Specifying the variables like this allows us to make our transform attributes more readable.
+// var leftTextX = margin + tPadLeft;
+// var leftTextY = (height + labelArea) / 2 - labelArea;
+// // We add a second label group, this time for the axis left of the chart.
+// svg.append("g").attr("class", "yText");
+// // yText will allows us to select the group without excess code.
+// var yText = d3.select(".yText");
+// // Like before, we nest the group's transform attr in a function
+// // to make changing it on window change an easy operation.
+// function yTextRefresh() {
+//   yText.attr(
+//     "transform",
+//     "translate(" + leftTextX + ", " + leftTextY + ")rotate(-90)"
+//   );
+// }
+// yTextRefresh();
+// // Now we append the text.
+// // 1. Obesity
+// yText
+//   .append("text")
+//   .attr("y", -26)
+//   .attr("data-name", "obesity")
+//   .attr("data-axis", "y")
+//   .attr("class", "aText active y")
+//   .text("Obese (%)");
+// // 2. Smokes
+// yText
+//   .append("text")
+//   .attr("x", 0)
+//   .attr("data-name", "smokes")
+//   .attr("data-axis", "y")
+//   .attr("class", "aText inactive y")
+//   .text("Smokes (%)");
+// // 3. Lacks Healthcare
+// yText
+//   .append("text")
+//   .attr("y", 26)
+//   .attr("data-name", "healthcare")
+//   .attr("data-axis", "y")
+//   .attr("class", "aText inactive y")
+//   .text("Lacks Healthcare (%)");
